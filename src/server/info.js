5d681b09b6ea28d76cecee6a5ddc87eced623d9f
Misha0510 <misha0510@gmail.com>
2021-09-02 14:00:51 +0300

    m

--- src/server/info.js
+++ src/server/info.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 5000;
 express()
   .use(express.static(path.join(__dirname, 'dist')))
   .set('views', path.join(__dirname, 'dist'))
-  .set('view engine', '/index')
+  .set('view engine', '/dist')
   .get('/', (req, res) => res.render('/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
