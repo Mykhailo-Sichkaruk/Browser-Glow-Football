e6af010dacdc93d17bf76784e11d3972e168e179
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-06-21 18:12:33 +0200

    Decompose

--- src/server/ball.js
+++ src/server/ball.js
@@ -13,28 +13,27 @@ class Ball {
 
 	move(dt) {
 
-		const dy = dt * this.speed * Math.cos(this.direction);
-		const dx = dt * this.speed * Math.sin(this.direction);
+		const shift = dt * this.speed;
+		const dy = shift * Math.cos(this.direction);
+		const dx = shift * Math.sin(this.direction);
 
 		//Check if ball hits the border then change direction
-		if        (this.y + this.radius + dy >= PITCH.BOTTOM_BORDER) {
+		if (this.hitBottom(dy) || this.hitTop(dy)) {
 			this.direction = Math.PI - this.direction;
-		} else if (this.y - this.radius + dy <= PITCH.TOP_BORDER) {
-			this.direction = Math.PI - this.direction;
-		} else if (this.x + this.radius + dx >= PITCH.RIGHT_BORDER) {
+		} else if (this.hitRight(dx)) {
 			if (this.isInGoal())
 				return 1;
 			else
 				this.direction = Math.PI * 2 - this.direction;
-		} else if (this.x - this.radius + dx <= PITCH.LEFT_BORDER) {
+		} else if (this.hitLeft(dx)) {
 			if (this.isInGoal())
 				return 2;
 			else
 				this.direction = Math.PI * 2 - this.direction;
 		}
 		//Then move
-		this.x += dt * this.speed * Math.sin(this.direction);
-		this.y += dt * this.speed * Math.cos(this.direction);
+		this.x += shift * Math.sin(this.direction);
+		this.y += shift * Math.cos(this.direction);
 		//Apply resistance to velosity
 		this.speed *= this.resistance;
 		return 0; //False for Ball not touching the border or goal
@@ -44,6 +43,27 @@ class Ball {
 		if ((this.y >= PITCH.FULL_Y / 2 - PITCH.GOAL_WIDTH / 2) && (this.y <= PITCH.FULL_Y / 2 + PITCH.GOAL_WIDTH / 2))
 			return true;
 	}
+
+	hitBottom(dy) {
+		if (this.y + this.radius + dy >= PITCH.BOTTOM_BORDER)
+			return true;
+	}
+
+	hitTop(dy) {
+		if (this.y - this.radius + dy <= PITCH.TOP_BORDER)
+			return true;
+	}
+
+	hitRight(dx) {
+		if (this.x + this.radius + dx >= PITCH.RIGHT_BORDER)
+			return true;
+	}
+
+	hitLeft(dx) {
+		if (this.x - this.radius + dx <= PITCH.LEFT_BORDER)
+			return true;
+	}
+
 }
 
 module.exports = Ball;
