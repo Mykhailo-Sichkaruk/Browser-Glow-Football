87ebcb89e1080a97d5ce09983fbbcdb3e7c7a3c4
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-06-28 19:34:03 +0200

    Add JsDocs, add RESISTANCE_ON_ASSIST

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,16 +1,23 @@
 // eslint-disable-next-line strict
 "use strict";
 const GAME = Object.freeze({
-	LEFT_TEAM: "left",
-	RIGHT_TEAM: "right",
-	AFTER_GOAL_DELAY_MS: 1000, //ms
-	PING_ON_KEY_STATUS_REFRESHED_MS: 50,
+	/** @type {number}  `miliseconds` Delay between server calculations of new game status */
 	SERVER_PING: 20,
-	SHOT_ANIMATION_TIME: 500, //ms
-	get PERFECT_SHOT_TIME() { return this.SHOT_ANIMATION_TIME / 4; },
-	SHOT_POWER_PANE_DELAY: 1000, //ms
-	MOUSE_UPDATE_DELAY: 100, //ms
+	/** @type {number} `miliseconds` Delay between sending info about key`s status */
+	PING_ON_KEY_STATUS_REFRESHED: 50,
+	/** @type {number} `miliseconds` Delay between sending info about mouse direction */
+	MOUSE_UPDATE_DELAY: 100,
+	/** @type {number} `miliseconds` Pause after goal is scored */
+	AFTER_GOAL_DELAY: 1000,
+	/** @type {number} `miliseconds` All time for possible shoot */
+	SHOT_ANIMATION_TIME: 500,
+	/** @type {number} `miliseconds` Appearence of shot panel after shot was made  */
+	SHOT_POWER_PANE_DELAY: 1000,
+	/** @type {number} Max players count in team in one game */
 	TEAM_MAX_PLAYERS: 3,
+	/** @type {number} `miliseconds` Timing to get perfect shot power */
+	get PERFECT_SHOT_TIME() { return this.SHOT_ANIMATION_TIME / 4; },
+	/** @type {number} Max players count in one game  */
 	get MAX_PLAYERS() { return this.TEAM_MAX_PLAYERS * 2; },
 });
 const BALL = Object.freeze({
@@ -27,7 +34,7 @@ const PLAYER = Object.freeze({
 	SPEED_ON_HOLD: 75,
 	PULL_FORCE: 6,
 	KICK_FORCE: 1700,
-	ASSIST_FORCE: 800,
+	ASSIST_FORCE: 650,
 	/*** Distance between Player and Ball where pull power works*/
 	get PULL_DISTANCE_POWER() 		{ return (this.RADIUS * 10) ** 2; },
 	get PLAYER_COLLISION_DISTANCE() { return (this.RADIUS * 2) ** 2; },
@@ -47,20 +54,21 @@ const PITCH = Object.freeze({
 	get TOP_BORDER()	{ return this.PADDING_WIDTH + this.OUTLINE_WIDTH; },
 	get BOTTOM_BORDER() { return this.PADDING_WIDTH + this.OUTLINE_WIDTH + this.Y; },
 	get RIGHT_BORDER()	{ return this.PADDING_WIDTH + this.OUTLINE_WIDTH + this.X; },
-	get LEFT_BORDER() { return this.PADDING_WIDTH + this.OUTLINE_WIDTH; },
-	get RED_GOALKEEPER_POSITION_X() { return this.RIGHT_BORDER - this.RADIUS * 2; },
-	get BLUE_GOALKEEPER_POSITION_X() { return this.LEFT_BORDER + this.RADIUS * 2; },
-	get RED_MIDFIELDER_POSITION_X() { return this.FULL_X / 4 * 3; },
-	get BLUE_MIDFIELDER_POSITION_X() { return this.FULL_X / 4; },
-	get RED_FORWARD_POSITION_X() { return this.FULL_X / 2 + this.CENTRAL_CIRCLE_RADIUS; },
-	get BLUE_FORWARD_POSITION_X() { return this.FULL_X / 2 - this.CENTRAL_CIRCLE_RADIUS; },
-	GOAL_WIDTH: 100, //WIDTH
+	get LEFT_BORDER() 	{ return this.PADDING_WIDTH + this.OUTLINE_WIDTH; },
+	get RED_GOALKEEPER_POSITION_X() 	{ return this.RIGHT_BORDER - this.RADIUS * 2; },
+	get BLUE_GOALKEEPER_POSITION_X() 	{ return this.LEFT_BORDER + this.RADIUS * 2; },
+	get RED_MIDFIELDER_POSITION_X()		{ return this.FULL_X / 4 * 3; },
+	get BLUE_MIDFIELDER_POSITION_X()	{ return this.FULL_X / 4; },
+	get RED_FORWARD_POSITION_X() 	{ return this.FULL_X / 2 + this.CENTRAL_CIRCLE_RADIUS; },
+	get BLUE_FORWARD_POSITION_X() 	{ return this.FULL_X / 2 - this.CENTRAL_CIRCLE_RADIUS; },
+	GOAL_WIDTH: 100,
 	COLOR: "rgb(0, 255, 64)",
 	CANVAS_BACKGROUND_COLOR: "rgb(255, 187, 0)",
 	OUTLINE_COLOR: "white",
 	CENTRAL_CIRCLE_RADIUS: 160,
 	BORDER_LEFT_X: 0,
-	RESISTANCE: 0.99,
+	RESISTANCE_DEFAULT: 0.99,
+	RESISTANCE_ON_ASSIST: 1,
 });
 const MESSAGE = Object.freeze({
 	CONNECTION: "connection",
